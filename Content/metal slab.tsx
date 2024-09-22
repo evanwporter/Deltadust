9107331ae0a26c224d701a75f86ba281070803ef
evanwporter <evanwporter@gmail.com>
2024-09-22 13:12:49 -0700

    Removed all

--- Content/metal slab.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="metal slab" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../Downloads/metal slab.svg" width="32" height="32"/>
-</tileset>
